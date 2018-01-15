ec10b084e36c18d9da53432a2172ae2059dadf8f
Trong Nguyen Van <trongnguyenvann@gmail.com>
2018-01-16 00:21:15 +0700

    Hello, this is the second file

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int displayCmd() {
+    cout << "Enter a number: ";
+    int number;
+    cin >> number;
+    cout << "You've just write " << number << endl;
+}
+
+int main() {
+    displayCmd();
+    getch();
+}
